19fd5535376637f4e529a7d3b745ff35b6c0da53
STD <mattiyahu11@gmail.com>
2019-10-18 19:17:08 +0800

    Added custom build

--- src/ckeditor.js
+++ src/ckeditor.js
@@ -7,10 +7,10 @@
 import ClassicEditorBase from '@ckeditor/ckeditor5-editor-classic/src/classiceditor';
 
 import Essentials from '@ckeditor/ckeditor5-essentials/src/essentials';
-import UploadAdapter from '@ckeditor/ckeditor5-adapter-ckfinder/src/uploadadapter';
 import Autoformat from '@ckeditor/ckeditor5-autoformat/src/autoformat';
 import Bold from '@ckeditor/ckeditor5-basic-styles/src/bold';
 import Italic from '@ckeditor/ckeditor5-basic-styles/src/italic';
+import Underline from '@ckeditor/ckeditor5-basic-styles/src/underline';
 import BlockQuote from '@ckeditor/ckeditor5-block-quote/src/blockquote';
 import CKFinder from '@ckeditor/ckeditor5-ckfinder/src/ckfinder';
 import EasyImage from '@ckeditor/ckeditor5-easy-image/src/easyimage';
@@ -28,15 +28,19 @@ import PasteFromOffice from '@ckeditor/ckeditor5-paste-from-office/src/pastefrom
 import Table from '@ckeditor/ckeditor5-table/src/table';
 import TableToolbar from '@ckeditor/ckeditor5-table/src/tabletoolbar';
 
+/* Added Plugins */
+import Base64UploadAdapter from '@ckeditor/ckeditor5-upload/src/adapters/base64uploadadapter';
+import Alignment from '@ckeditor/ckeditor5-alignment/src/alignment';
+
 export default class ClassicEditor extends ClassicEditorBase {}
 
 // Plugins to include in the build.
 ClassicEditor.builtinPlugins = [
 	Essentials,
-	UploadAdapter,
 	Autoformat,
 	Bold,
 	Italic,
+	Underline,
 	BlockQuote,
 	CKFinder,
 	EasyImage,
@@ -52,7 +56,11 @@ ClassicEditor.builtinPlugins = [
 	Paragraph,
 	PasteFromOffice,
 	Table,
-	TableToolbar
+	TableToolbar,
+
+	/* Added plugins */
+	Base64UploadAdapter,
+	Alignment,
 ];
 
 // Editor configuration.
@@ -63,6 +71,8 @@ ClassicEditor.defaultConfig = {
 			'|',
 			'bold',
 			'italic',
+			'underline',
+			'alignment',
 			'link',
 			'bulletedList',
 			'numberedList',
@@ -74,14 +84,31 @@ ClassicEditor.defaultConfig = {
 			'redo'
 		]
 	},
+	
 	image: {
 		toolbar: [
+			'imageStyle:alignLeft',
 			'imageStyle:full',
-			'imageStyle:side',
+			'imageStyle:alignRight',
 			'|',
 			'imageTextAlternative'
-		]
+		],
+		styles: [
+            // This option is equal to a situation where no style is applied.
+            'full',
+
+            // This represents an image aligned to the left.
+            'alignLeft',
+
+            // This represents an image aligned to the right.
+            'alignRight'
+        ]
 	},
+	
+	alignment: {
+        options: [ 'left', 'center', 'right', 'justify' ]
+    },
+
 	table: {
 		contentToolbar: [
 			'tableColumn',
